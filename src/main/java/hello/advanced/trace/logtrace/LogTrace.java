f381ef28893e50d8743ed1c6fd3f62fe4972e219
zingwang <zingwang06@naver.com>
2023-03-21 20:09:33 +0900

    ThreadLocal 필드동기화

---
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -0,0 +1,7 @@
+package hello.advanced.trace.logtrace;
+import hello.advanced.trace.TraceStatus;
+public interface LogTrace {
+    TraceStatus begin(String message);
+    void end(TraceStatus status);
+    void exception(TraceStatus status, Exception e);
+}
