a75cc0cc0bb3fe4e80e27fcddd25c49a03a19aad
zingwang <zingwang06@naver.com>
2023-03-26 13:35:37 +0900

    CGLIB - 예제 코드

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 
 //\\@Import({AppV2Config.class,AppV1Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
-@Import(DynamicProxyBasicConfig.class)//dynamic proxy
+@Import(DynamicProxyFilterConfig.class)//dynamic proxy
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
