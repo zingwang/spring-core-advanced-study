0316718cb2918086be4ef9d00a4a03bb45e86c43
zingwang <zingwang06@naver.com>
2023-03-30 23:45:30 +0900

    스프링이 제공하는 빈 후처리기

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -9,6 +9,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -22,8 +23,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyFilterConfig.class)//dynamic proxy
 //@Import(ProxyFactoryConfigV1.class)//dynamic proxy
 //@Import(ProxyFactoryConfigV2.class)//dynamic proxy
-@Import(BeanPostProcessorConfig.class)//dynamic proxy
-
+// @Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
