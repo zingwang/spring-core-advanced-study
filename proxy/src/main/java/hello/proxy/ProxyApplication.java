5a57c5d8236d14aa6847c63d30b9810c6d8ead9b
zingwang <zingwang06@naver.com>
2023-03-30 22:50:23 +0900

    빈 후처리기 - 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,8 +8,10 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,9 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import(DynamicProxyFilterConfig.class)//dynamic proxy
 //@Import(ProxyFactoryConfigV1.class)//dynamic proxy
-@Import(ProxyFactoryConfigV2.class)//dynamic proxy
+//@Import(ProxyFactoryConfigV2.class)//dynamic proxy
+@Import(BeanPostProcessorConfig.class)//dynamic proxy
+
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
